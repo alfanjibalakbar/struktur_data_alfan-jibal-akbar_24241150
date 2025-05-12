705819a2ac28dc16bf23bea5444a5876fd664555
Alfan jibal akbar <jibalalfan08@gmail.com>
2025-05-12 18:43:57 +0800

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,11 @@
+#pengecekan bilangan dalam python def chek_number(bilangan):
+if bilangan < 5 or bilangan > 10:
+    else:
+    print(" < 5 or > 10 ")
+
+    #meminta input dari penguna
+    try:
+        num = int(input("5 : "))
+        check_number(num)
+        except ValueError
+    print("10.") 
